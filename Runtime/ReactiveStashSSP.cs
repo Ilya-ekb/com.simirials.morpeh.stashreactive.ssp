4d2e5600aa4e91f1178d55f8319e3885f0b1127f
ilyapolovinkin <2190055@gmail.com>
2025-09-01 22:58:10 +0300

    fix system group adding

--- Runtime/ReactiveStashSSP.cs
+++ Runtime/ReactiveStashSSP.cs
@@ -1,15 +1,15 @@
+// MIT License
 using System;
 using Scellecs.Morpeh;
 using Scellecs.Morpeh.Systems;
 using Unity.IL2CPP.CompilerServices;
 
 namespace Morpeh.ReactiveSSP {
+    /// Реактивные события на вход/выход сущности в Filter.With<T>().
     [Il2CppSetOption(Option.NullChecks, false)]
     [Il2CppSetOption(Option.ArrayBoundsChecks, false)]
     [Il2CppSetOption(Option.DivideByZeroChecks, false)]
-    public sealed class ReactiveStashSSP<T> : ISystem where T : struct, IComponent {
-        public World World { get; set; }
-
+    public sealed class ReactiveStashSSP<T> : UpdateSystem where T : struct, IComponent {
         public event Action<Entity> Added;
         public event Action<Entity> Removed;
 
@@ -20,36 +20,43 @@ namespace Morpeh.ReactiveSSP {
             this.emitExistingOnAwake = emitExistingOnAwake;
         }
 
-        public void OnAwake() {
-            processor = World.Filter.With<T>().ToSystemStateProcessor(Create, Remove);
-            if (emitExistingOnAwake) processor.Process();
+        public override void OnAwake() {
+            processor = World.Filter.With<T>()
+                .ToSystemStateProcessor(Create, Remove);
+            if (emitExistingOnAwake) {
+                processor.Process(); // разовая обработка уже существующих
+            }
         }
 
-        public void OnUpdate(float dt) => processor.Process();
+        public override void OnUpdate(float deltaTime) => processor.Process();
 
-        public void Dispose() {
+        public override void Dispose() {
             processor.Dispose();
             Added = null;
             Removed = null;
         }
 
+        // Вход в фильтр (добавили T / ожила сущность)
         private State Create(Entity e) {
             Added?.Invoke(e);
             return new State { entity = e };
         }
 
+        // Выход из фильтра или уничтожение сущности
         private void Remove(ref State s) => Removed?.Invoke(s.entity);
 
-        private struct State : ISystemStateComponent { public Entity entity; }
+        private struct State : ISystemStateComponent {
+            public Entity entity;
+        }
     }
 
     public static class ReactiveStashSSPExtensions {
-        public static ReactiveStashSSP<T> AddReactiveStashSSP<T>(this World world, int order = 0, bool emitExistingOnAwake = true)
+        public static ReactiveStashSSP<T> AddReactiveStashSSP<T>(this World world, bool emitExistingOnAwake = true)
             where T : struct, IComponent {
             var sys = new ReactiveStashSSP<T>(emitExistingOnAwake);
             var group = world.CreateSystemsGroup();
             group.AddSystem(sys);
-            world.AddSystemsGroup(order, group);
+            world.AddPluginSystemsGroup(group);
             return sys;
         }
     }
